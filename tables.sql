47127de93d22b276660c6392a47a467d2e9b6d27
jhaylava <122855485+jhaylava@users.noreply.github.com>
2024-01-31 18:53:47 +0700

    Update tables.sql

--- tables.sql
+++ tables.sql
@@ -1,35 +1,35 @@
 CREATE TABLE users (
-	user_id SERIAL PRIMARY KEY,
-	first_name VARCHAR(50),
-	last_name VARCHAR(50),
-	email VARCHAR(100),
-	password VARCHAR(50),
+	user_id SERIAL PRIMARY KEY NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	email VARCHAR(100) NOT NULL,
+	password VARCHAR(50) NOT NULL,
 	imageURL VARCHAR(255)
 );
 
 CREATE TABLE houses (
-	house_id SERIAL PRIMARY KEY,
-	location VARCHAR(50),
-	bedrooms INT,
-	bathrooms INT,
-	price_night INT,
-	description VARCHAR(2500),
+	house_id SERIAL PRIMARY KEY NOT NULL,
+	location VARCHAR(50) NOT NULL,
+	bedrooms INT NOT NULL,
+	bathrooms INT NOT NULL,
+	price_night INT NOT NULL,
+	description VARCHAR(2500) NOT NULL,
 	user_id INT REFERENCES users(user_id) 
 	-- ask if int or serial during references -- 
 );
 
 CREATE TABLE bookings (
 	user_id SERIAL REFERENCES users(user_id), -- ask-
-	booking_id SERIAL PRIMARY KEY,
-	price_night INT,
-	total_price INT
+	booking_id SERIAL PRIMARY KEY NOT NULL,
+	price_night INT NOT NULL,
+	total_price INT NOT NULL
 );
 
 CREATE TABLE reviews (
-	review_id SERIAL PRIMARY KEY,
-	first_name VARCHAR(50),
-	last_name VARCHAR(50),
-	review_description VARCHAR(2500),
+	review_id SERIAL PRIMARY KEY NOT NULL,
+	first_name VARCHAR(50) NOT NULL,
+	last_name VARCHAR(50) NOT NULL,
+	review_description VARCHAR(2500) NOT NULL,
 	star_rating FLOAT,
 	review_date DATE,
 	user_id INT REFERENCES users(user_id), -- asks--
@@ -38,7 +38,7 @@ CREATE TABLE reviews (
 );
 
 CREATE TABLE house_photos (
-	photo_id SERIAL PRIMARY KEY,
+	photo_id SERIAL PRIMARY KEY NOT NULL,
 	house_id INT REFERENCES houses(house_id),
 	photo_url imageURL VARCHAR(255),
 	feature_photo BOOLEAN,
