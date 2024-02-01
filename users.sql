f2251c1fd1f8312fe32128e29a724f7e717081a2
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-02-01 18:39:38 +0700

    profile users

--- users.sql
+++ users.sql
@@ -1,2 +1,4 @@
---login profile users
-SELECT profile_picture, first_name, last_name, email FROM users;
+--update profile users
+UPDATE users
+SET profile_picture = '', first_name = '', last_name = '', email = '', password = '' 
+WHERE user_id = 1234;
