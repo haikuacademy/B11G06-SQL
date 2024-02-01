104faa9a0a63133b6686fac93992046c8214fbbd
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-02-01 16:20:52 +0700

    inserting values on bookings table

--- users.sql
+++ users.sql
@@ -1,28 +1,12 @@
---create table users
-CREATE TABLE users (
-    user_id SERIAL PRIMARY KEY NOT NULL,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    email VARCHAR(100) NOT NULL,
-    password VARCHAR(50) NOT NULL,
-    profile_pictureURL VARCHAR(255)
-);
-
---insert users
-INSERT INTO users (user_id, first_name, last_name, email, password, profile_pictureURL)
-VALUES ('Bob', 'Smith','bobsmith@gmail.com﻿', '1234BOB﻿', 'https://www.fakepersongenerator.com/Face/male/male20161083948806152.jpg');
+INSERT INTO bookings (price_night, booking_date, message, total_price, user_id)
+VALUES 
+	(250, '2024-12-20', 'hello there!', 500, 2),
+	(100, '2024-12-20', 'hello there!', 1256, 4),
+	(100, '2024-12-20', 'hello there!', 1247, 3),
+	(300, '2024-12-20', 'hello there!', 300, 5),
+	(500, '2024-12-20', 'hello there!', 256, 6);
 
 
---read login users
-SELECT email, password FROM users;
 
 
---login profile users
-SELECT profile_picture, first_name, last_name, email FROM users;
-
-
---update profile users
-UPDATE users
-SET profile_picture = '', first_name = '', last_name = '', email = '', password = '' 
-WHERE user_id = 1234;
 
