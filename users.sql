437e5d0b3e4ef24d71f276ffb559ff6d01fff0d6
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-02-01 14:40:15 +0700

    creating the users table

--- users.sql
+++ users.sql
@@ -1,5 +1,15 @@
+--create table users
+CREATE TABLE users (
+    user_id SERIAL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(50) NOT NULL,
+    profile_pictureURL VARCHAR(255)
+);
+
 --insert users
-INSERT INTO users (first_name, last_name, email, password, profile_picture)
+INSERT INTO users (user_id, first_name, last_name, email, password, profile_pictureURL)
 VALUES ('Bob', 'Smith','bobsmith@gmail.com﻿', '1234BOB﻿', 'https://www.fakepersongenerator.com/Face/male/male20161083948806152.jpg');
 
 
