d762fb6174ecea07c2a29374979d578cef0ac14f
SlviACH6 <146087759+SlviACH6@users.noreply.github.com>
2024-02-13 19:43:44 +0700

    Query for users table

--- bookings.sql
+++
@@ -1,9 +0,0 @@
---inserting data into bookings table
-INSERT INTO bookings (price_night, booking_date, message, total_price, user_id)
-VALUES 
-	(250, '2024-12-20', 'hello there!', 500, 2),
-	(100, '2024-12-20', 'hello there!', 1256, 4),
-	(100, '2024-12-20', 'hello there!', 1247, 3),
-	(300, '2024-12-20', 'hello there!', 300, 5),
-	(500, '2024-12-20', 'hello there!', 256, 6);
-
--- users.sql
+++ users.sql
@@ -1,4 +1,16 @@
---update profile users
-UPDATE users
-SET profile_picture = '', first_name = '', last_name = '', email = '', password = '' 
-WHERE user_id = 1234;
+--USERS register create*/
+INSERT INTO users (first_name, last_name, email, password, profile_picture)
+VALUES (Bob, Sponge, bob.sponge@gmail.com, 123456, https://randomuser.me/api/portraits/men/90.jpg),
+    (Man, Super, superM@gmail.com, 111222, https://randomuser.me/api/portraits/men/68.jpg),
+    (Snow, Jhon, white@gmail.com, 123789, https://randomuser.me/api/portraits/men/67.jpg),
+    (Solo, Hans, yosolo@yahoo.com, 123963, https://randomuser.me/api/portraits/men/69.jpg),
+    (Woman, Wonder, wondering@hotmail.com, 112566, https://randomuser.me/api/portraits/women/37.jpg)
+
+--USERS login
+SELECT email, password FROM users
+
+--USERS read profile
+SELECT profile_picture, first_name, last_name, email FROM users
+
+--USERS update profile
+UPDATE profile_picture, first_name, last_name; email FROM USERS
